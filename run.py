89c0f27d5ad0c0d441fee32522e57b8c8ada30d6
apiraccini <alepiraccini@gmail.com>
2025-02-09 17:21:36 +0100

    Add logic for matching question data with recipes

--- run.py
+++ run.py
@@ -1,26 +1,36 @@
 import json
+from pathlib import Path
 
-import pandas as pd
-
-from src.utils.questions import process_questions
-
-
-def process_questions_pipeline():
-    """
-    Reads questions from a CSV file, processes them, and returns the processed questions.
-    Returns:
-        dict: A dictionary containing the processed questions.
-    """
-
-    questions_df = pd.read_csv("data/raw/domande.csv")
-    processed_questions_list = process_questions(questions_df=questions_df)
-
-    return processed_questions_list
+from src.pipeline.questions import process_questions_pipeline
+from src.pipeline.recipes import match_recipes_pipeline, process_recipes_pipeline
 
 
 def main():
-    processed_questions = process_questions_pipeline()
-    json.dumps(processed_questions[0], indent=4)
+    """Main pipeline"""
+
+    # Define paths
+    data_path = Path("data")
+    questions_path = data_path / "raw/domande.csv"
+    parsed_questions_path = data_path / "processed/parsed_questions.json"
+    recipes_mapping_path = data_path / "raw/Misc/dish_mapping.json"
+
+    # Process questions
+    questions_data = process_questions_pipeline(
+        input_path=questions_path, output_path=parsed_questions_path
+    )
+
+    # Process recipes
+    recipes_data = process_recipes_pipeline()
+    recipes_mapping = json.load(open(recipes_mapping_path))
+
+    questions_recipes_mapped = match_recipes_pipeline(
+        output_path="data/processed/questions_with_recipes.json",
+        recipes_data=recipes_data,
+        questions_data=questions_data,
+        mapping=recipes_mapping,
+    )
+
+    print(json.dumps(questions_recipes_mapped, indent=4))
 
 
 if __name__ == "__main__":
