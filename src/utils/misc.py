b9b89e5003c1877325fa306684a882c4ebe10bd9
apiraccini <alepiraccini@gmail.com>
2025-02-14 23:45:31 +0100

    update roman_to_int function

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -126,15 +126,20 @@ def roman_to_int(roman: str | int) -> int:
     if isinstance(roman, int):
         return roman
 
-    roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     roman = roman.upper()
+    if roman == "0":
+        return 0
+
+    roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     result = 0
     prev_value = 0
     for char in reversed(roman):
+        if char == "+":
+            continue
         value = roman_numerals.get(char, 0)
         if value < prev_value:
             result -= value
         else:
             result += value
         prev_value = value
-    return result if result > 0 else 0
+    return result if result >= 0 else 0
